bec37e6db9631ccebac215a3f9ec2eda0ec54bba
rohini287 <hosurkarrohini@gmail.com>
2021-01-08 17:48:04 +0530

    Add the image

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gamestate="onSling"
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,12 +69,16 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gamestate==="onSling"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+   
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gamestate="launched"
 }
 
 function keyPressed(){
